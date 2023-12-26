16dee6ad73a0579b125d4c371db539c468ca5518
Omprakash <ompdhakar27@gmail.com>
2023-12-26 19:02:20 +0530

    create logger and exception file

--- setup.py
+++ setup.py
@@ -21,11 +21,10 @@ def get_requirements(file_path:str)->List[str]:
 
 
 setup(
-    name = "mlproject",
-    version = '0.0.1',
-    author = "omprakash",
-    author_email = "omprakash123@gmail.com",
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
-
+name = "mlproject",
+version = '0.0.1',
+author = "omprakash",
+author_email = "omprakash123@gmail.com",
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
 )
